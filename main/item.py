6018313ab4f26dd8a326c6bcb4c3e1248dd49bce
Nafis Ahsan <nafis01@students.cs.ubc.ca>
2022-02-18 16:57:53 -0800

    implemented hashmap log and checking expiry dates

--- main/item.py
+++ main/item.py
@@ -2,13 +2,23 @@ from datetime import date
 
 class Item:
 
+# constructor of item
+# @param name - the name of the product
+# @param price - the price of the product
+# @param expDate - the expiration date of the product
+# @type {{name : string, price : double, expDate : date} => Item}
+
     def __init__(self, name, price, expDate):
         self.name = name
         self.price = price
         self.date = expDate
 
-    def isExpired(self):
-        d1 = date.today.strftime("%d")
-        print("d1 =", d1)  
+# Checks the current date and returns true if the food has expired; expired food has an expiry date later than the current date
+# @type {=> boolean}
 
-        return (self.date.day > d1)
+    def isExpired(self):
+        dayToday = date.today.strftime("%-d")
+        monthToday = date.today.strftime("%-m")
+        yearToday = date.today.strftime("%Y")
+
+        return self.date.isEarlier(dayToday, monthToday, yearToday)
