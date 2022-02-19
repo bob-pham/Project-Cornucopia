0092fcf6237e81e0105230dbb42206b67eea9622
Bob Pham <bobbaopham@gmail.com>
2022-02-19 01:20:24 -0800

    added documentation for AccuracyTest

--- main/item.py
+++ main/item.py
@@ -1,22 +1,31 @@
 from datetime import date
+from sqlite3 import Date
+
+from numpy import double
 
 class Item:
+    # TODO description of item class
 
-# constructor of item
-# @param name - the name of the product
-# @param price - the price of the product
-# @param expDate - the expiration date of the product
-# @type {{name : string, price : double, expDate : date} => Item}
 
-    def __init__(self, name, price, expDate):
+    def __init__(self, name: str, price: double, expDate: date):
+        """constructor of item
+
+        Args:
+            name (_type_): the name of the product
+            price (_type_): the price of the product
+            expDate (_type_): the expiration date of the product
+        """
         self.name = name
         self.price = price
         self.date = expDate
 
-# Checks the current date and returns true if the food has expired; expired food has an expiry date later than the current date
-# @type {=> boolean}
 
-    def isExpired(self):
+    def isExpired(self) -> bool:
+        """Checks the current date and returns true if the food has expired; expired food has an expiry date later than the current date
+
+        Returns:
+            bool: returns true if expired, false otherwise
+        """
         dayToday = date.today.strftime("%-d")
         monthToday = date.today.strftime("%-m")
         yearToday = date.today.strftime("%Y")
