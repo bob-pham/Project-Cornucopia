2ff4938b5a69f58a32dbf86836a647f3e9727bb2
Bob Pham <bobbaopham@gmail.com>
2022-05-18 23:40:22 -0700

    json magic

--- recall_scraper/fda_recalls.py
+++ recall_scraper/fda_recalls.py
@@ -1,7 +1,18 @@
 import requests
 
-response = requests.get('https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[2022-01-01+TO+2022-02-11]&limit=20')
-print(response.json())
+# response = requests.get('https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[2022-01-01+TO+2022-02-11]&limit=20')
+# print(response)
+# print(response.json())
 
 #https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[2022-01-01+TO+2022-02-11]&limit=20
 
+def get_food_recalls():
+    response = requests.get('https://api.fda.gov/food/enforcement.json?search=recall_initiation_date:[2022-01-01+TO+2022-02-11]&limit=20')
+    
+    if response:
+        response = response.json()
+        print(response.results)
+    else:
+        raise Exception("Failed to get response from FDA")
+
+get_food_recalls()
