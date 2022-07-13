68b98b6fced73771c9c4a155c573b81e4255c29d
Bob Pham <bobbaopham@gmail.com>
2022-07-12 17:37:47 -0700

    frontend and backend connected, ironing out bugs

--- cornucopia/frontend/src/App.jsx
+++ cornucopia/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import logo from './logo.svg'
-import '../dist/output.css'
+// import '../dist/output.css'
 import Login from './Login'
 import Homescreen from './Homescreen'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
